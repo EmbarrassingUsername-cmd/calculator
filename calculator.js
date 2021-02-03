ccf7d78752503594cbd7de632ce93d1cf28f8c9d
Jamie Bukraba <Jamie.Bukraba@googlemail.com>
2021-02-03 20:41:53 +0000

    added keyboard support for both num pad and num row as well as operators added semi colons where missing

--- calculator.js
+++ calculator.js
@@ -17,6 +17,7 @@ numberButton.forEach(element=> element.addEventListener("click", numberFunction)
 equalsButton.addEventListener("click", evaluateFunction);
 clearButton.addEventListener("click", clearFunction);
 delButton.addEventListener("click",deleteFunction);
+window.addEventListener("keydown", keyboardFunction)
 function clearFunction(){
     displayEntry.innerText="";
     displayCalc.innerText="";
@@ -27,6 +28,17 @@ function clearFunction(){
     repeatFunction="";
     repeatSecond="";
 };
+//keyboardfunction allows for key presses to activate buttons added 2 data key properties to support num pad and normal number keys
+function keyboardFunction(e){
+    const key=document.querySelector(`button[data-key="${e.keyCode}"]`);
+    if(!key){
+        const key2=document.querySelector(`button[data-key2="${e.keyCode}"]`);
+        if(key2) key2.click();
+        return;
+    }
+    key.click();
+}
+
 function deleteFunction(){
     if(secondValue){
         secondValue=secondValue.slice(0,-1);
@@ -89,21 +101,22 @@ function operatorFunction(){
     displayCalc.innerText=firstValue+" "+operator 
     displayEntry.innerText=secondValue
 };
+//number function will used when number/decimal is pressed and will change the value stings accordingly
 function numberFunction(){
     if(operatorValue){
-        if (secondValue.length<13){
-            //checks for previous use of decimal. ID cannot contain a . due to CSS
+        if (secondValue.length<16){
             if (this.id.charAt(this.id.length-1)=="l"){
-                if(!secondValue.indexOf(".")) secondValue+="."
-            else return alert("decimal point already used")
-            };
-            secondValue+=this.id.charAt(this.id.length-1);
+                if(secondValue.indexOf(".")==-1) secondValue+="."
+                else return alert("decimal point already used")
+            }
+            else secondValue+=this.id.charAt(this.id.length-1);
             displayEntry.innerText=secondValue;
             return;
+
         }
-        else{alert("Please limit inputs to 12 characters"); return}
+        else{alert("Please limit inputs to 15 characters"); return}
     };
-    if (firstValue.length<13){
+    if (firstValue.length<16){
         if (this.id.charAt(this.id.length-1)=="l"){
             if((firstValue.indexOf(".")==-1)){
                 firstValue+="."
@@ -113,14 +126,14 @@ function numberFunction(){
     else firstValue+=this.id.charAt(this.id.length-1);
     displayEntry.innerText=firstValue;
     }
-    else alert("Please limit inputs to 12 characters");
-};
+    else alert("Please limit inputs to 15 characters");
+}; 
 function operate(operator,b,c){
     if (operator==="add")return add(b,c);
     if (operator==="subtract")return subtract(b,c);
     if (operator==="multiply")return multiply(b,c);
     if (operator==="divide"){
-        if(!(secondValue==="0"))return divide(b,c);
+    if(!(secondValue==="0"))return divide(b,c);
         else return alert("Please do not attempt division by zero this is a known bug with Universe v1.0.0 and successful division is not currently possible please consult with the developer of Universe to see when this will be patched. Universe has not seen an update in nearly 14 billion years and the expected 2012 patch never happened")
     }
     else{alert("There appears to have been a problem please alert the developper if you haven't been fiddling in the console")}
